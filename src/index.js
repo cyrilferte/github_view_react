4a660981d5cf30ebbc563dca4be34c7270e08ee9
Cyril Ferte <cyril@fertec.fr>
2017-01-07 10:52:58 +0000

    react first real app

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+
+import App from './components/App.jsx';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
